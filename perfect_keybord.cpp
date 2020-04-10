5487065e8567112e936a1edee130d8ff8fe3ea65
arpit gupta <arpit.cs18@nitp.ac.in>
2020-04-10 19:48:16 +0530

    awsome example of deque, got ac

--- perfect_keybord.cpp
+++ perfect_keybord.cpp
@@ -1,43 +1,71 @@
-// g++ perfect_keybord.cpp -o perfect_keybord && ./perfect_keybord
-
-/*
-
-
-*/
-
 #include <bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); cerr.tie(NULL)
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define endl '\n'
-#define ll long long int
+#define int long long int
 using namespace std;
-#define pii pair <ll , ll >
+#define pii pair <int, int>
+#define mii map<int, int>
 #define pb push_back
 #define deb(x) cout << #x << " " << x << endl
 #define deb2(x, y) cout << #x << " " << x << " " << #y << " " << y << endl
-#define loop(x) for (int i = 0; i < x; i++)
+#define Loop(s, e, itr) for (int itr = s; itr < e; itr++)
+#define loop(n) for(int i = 0; i < n; i++)
 #define vin vector<int>
-#define vll vector<long long>
+#define w(t) int tc; cin >> tc; for(int t = 1; t <= tc; t++)
+#define vec vector
+#define mk_arr(n, t, s) t* n = new t[s]; loop(s) cin >> n[i]; 
+#define mi_arr(n, s) int* n = new int[s]; loop(s) cin >> n[i]; 
+#define arr_out(n, s) Loop(0, s, lout) cout << n[lout] << " ";
+#define pi(x) printf("%lld ", x);
+#define end printf("\n");
 
-int main(){
 
-    fastio;    
-    int t;
+
+int32_t main(){
+    fastio;
     string str;
-    cin >> t;
-    while(t--){
+    int len;
+    w(t){
         cin >> str;
-        string ans = "";
-        int ans_last = -1, ans_first = -1;
-        int len = str.length();
-        vector<bool> selected(26, false);
-        vin next(26, -1), prev(26, -1);
-        for(int i = 0; i < len; i++){
-            int temp = str[i]='a';
-            if(!selected[temp]){
-                selected[temp] = true;
-                
+        len = str.length();
+        
+        deque<char> dq;
+        bool isThere[26];
+        memset(isThere, 0, sizeof(isThere));
+        
+        dq.pb(str[0]);
+        isThere[str[0]-'a'] = true;
+
+        bool ok = true;
+        Loop(1, len, i){
+            if(isThere[str[i]-'a']){
+                if(dq[0] == str[i] ) if(dq[1] != str[i-1]) ok = false;
+                Loop(1, dq.size()-1, j) if(dq[j] == str[i]){
+                    if(dq[j-1] != str[i-1] and dq[j+1] != str[i-1]) ok = false;
+                }
+                else if(dq.back() == str[i] and dq[dq.size()-2] != str[i-1]) ok = false;
+
+            }
+            else{
+                if(dq[0] == str[i-1]){
+                    dq.push_front(str[i]);
+                    isThere[str[i]-'a'] = true;
+                }
+                else if(dq.back() == str[i-1]){
+                    dq.pb(str[i]);
+                    isThere[str[i]-'a'] = true;
+                }
+                else ok = false;
             }
         }
+        if(ok){
+            cout << "YES" << endl;
+            for(auto i : dq) cout << i;
+            loop(26) if(!isThere[i]) cout << (char)(i+'a');
+            cout << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
     }
-
 }
