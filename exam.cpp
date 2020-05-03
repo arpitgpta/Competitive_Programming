9f49c819637ddcc76341d90801c268fd3819c9a5
arpit gupta <arpit.cs18@nitp.ac.in>
2020-05-03 22:33:38 +0530

    very easy problem

---
+++ exam.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define int long long int
+using namespace std;
+#define pii pair <int, int>
+#define mii map<int, int>
+#define pb push_back
+#define deb(x) cout << #x << " " << x << endl
+#define deb2(x, y) cout << #x << " " << x << " " << #y << " " << y << endl
+#define Loop(s, e, itr) for (int itr = s; itr < e; itr++)
+#define loop(n) for(int i = 0; i < n; i++)
+#define vin vector<int>
+#define w(t) int tc; cin >> tc; for(int t = 1; t <= tc; t++)
+#define vec vector
+#define mod 1000000007
+#define all(x) x.begin(), x.end()
+
+int32_t main(){
+    fastio;
+    int n;
+    cin >> n;
+    int last = 0;
+    vec<pii> days(n);
+    loop(n) cin >> days[i].first >> days[i].second;
+    sort(all(days));
+    loop(n){
+        if(last <= days[i].second) last = days[i].second;
+        else last = days[i].first;   
+    }
+    cout << last << endl;
+    
+}
