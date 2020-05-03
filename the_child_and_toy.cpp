09e59a8bb27fe43f8bb1a4b2d98c1e551095907a
arpit gupta <arpit.cs18@nitp.ac.in>
2020-05-03 19:06:40 +0530

    got ac with 2nd approch

--- the_child_and_toy.cpp
+++ the_child_and_toy.cpp
@@ -20,7 +20,10 @@ using namespace std;
 #define mod 1000000007
 
 
-int32_t main(){
+
+
+// approch 1
+int32_t main1(){
     int n, m;
     scanf("%lld", &n);
     scanf("%lld", &m);
@@ -34,6 +37,41 @@ int32_t main(){
         scanf("%lld", &t1);
         scanf("%lld", &t2);
         ans += min(arr[t1], arr[t2]);
+    }
+    cout << ans << endl;
+}
+
+//approch 2
+int32_t main(){
+    int n, m;
+    cin >> n >> m;
+    vec<pii> cst(n);
+    int temp;
+    int arr[n+1];
+    loop(n){
+        cin >> temp;
+        arr[i+1] = temp;
+        cst[i] = {temp, i+1};
+    }
+    sort(cst.begin(), cst.end());
+    reverse(cst.begin(), cst.end());
+    vec<vin> alst(n+1);
+    int temp2;
+    loop(m){
+        cin >> temp >> temp2;
+        alst[temp].pb(temp2);
+        alst[temp2].pb(temp);
+    }
+    vec<bool> avilable(n+1, true);
+    int ans = 0;
+    loop(n){
+        int v = cst[i].second;
+        // deb(v);
+        for(auto j : alst[v]){
+            if(avilable[j]) ans += arr[j];
+        }
+        avilable[v] = false;
+        // deb(ans);
     }
     cout << ans << endl;
 }
